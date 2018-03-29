e6d9b413d94bab6dc13ac44c0f452d4292d2e04e
junjun <postor@gmail.com>
2018-03-29 17:37:42 +0800

    Update 01.js

--- 00-frontend/algorithms/array/01.js
+++ 00-frontend/algorithms/array/01.js
@@ -3,6 +3,38 @@
  *    Implement an iterator that supports hasNext(), next() and remove() methods.
  */
 
+
+class MyIterator {
+  constructor(arr){
+    this.arr = arr
+    this.i = [0,0]
+  }
+  
+  next(){
+    if(!this.hasNext()){
+      return undefined
+    }
+    
+    let [i0,i1] = this.i    
+    const rtn = this.arr[i0][i1]
+    i1++
+    if(i1 == this.arr[i0].length){
+      i0++
+      i1=0
+    }
+    return rtn
+  }
+  
+  hasNext(){
+    const [i0,i1] = this.i
+    if(i0==this.arr.length-1 && i1 == this.arr[i0].length){
+      return false
+    }
+    return true
+  }
+}
+
+
 const arr = [[], [1, 2, 3], [4, 5], [], [], [6, 7], [8], [9, 10], [], []]
 
 const it = new MyIterator(arr)
@@ -10,9 +42,4 @@ const target = []
 while(it.hasNext()){
   target.push(it.next())
 }
-
-class MyIterator {
-  constructor(){
-    
-  }
-}
+console.log(target)
